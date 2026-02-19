06500bcf7c7721d823b45112960c88c2632ca0cc
Daniel Angell <me@danangell.com>
2026-02-18 20:57:52 -0800

    Use meta tag to reload page every 24 hours

--- src/views/alerts.tsx
+++ src/views/alerts.tsx
@@ -42,6 +42,7 @@ export function AlertsPage({ charges }: { charges: ChargeAlert[] }) {
             content="width=device-width, initial-scale=1.0"
           />
           <title>Donation Alerts | Noisebridge</title>
+          <meta http-equiv="refresh" content="86400" />
           <link rel="icon" href="/assets/image/favicon.svg" />
           <link rel="stylesheet" href="/assets/css/reset.css" />
           <link rel="stylesheet" href="/assets/css/alerts.css" />
