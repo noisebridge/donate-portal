53c285f0bffaedebd80bde14f6afe2afd1585147
Daniel Angell <me@danangell.com>
2026-05-20 15:54:23 -0700

    Ensure use(page) is called in fixtures.ts

--- e2e/fixtures.ts
+++ e2e/fixtures.ts
@@ -2,38 +2,36 @@ import { test as base } from "@playwright/test";
 
 export const test = base.extend({
   page: async ({ page, baseURL }, use) => {
-    if (!baseURL) {
-      return;
-    }
-
     const errors: string[] = [];
 
-    page.on("pageerror", (error) => {
-      if (!page.url().startsWith(baseURL)) {
-        return;
-      }
+    if (baseURL) {
+      page.on("pageerror", (error) => {
+        if (!page.url().startsWith(baseURL)) {
+          return;
+        }
 
-      errors.push(`Uncaught: ${error.message}`);
-    });
+        errors.push(`Uncaught: ${error.message}`);
+      });
 
-    page.on("console", (msg) => {
-      if (msg.type() !== "error") {
-        return;
-      }
+      page.on("console", (msg) => {
+        if (msg.type() !== "error") {
+          return;
+        }
 
-      // Ignore errors on third-party pages
-      if (!page.url().startsWith(baseURL)) {
-        return;
-      }
+        // Ignore errors on third-party pages
+        if (!page.url().startsWith(baseURL)) {
+          return;
+        }
 
-      // Ignore errors originating from third-party scripts
-      const location = msg.location();
-      if (location.url && !location.url.startsWith(baseURL)) {
-        return;
-      }
+        // Ignore errors originating from third-party scripts
+        const location = msg.location();
+        if (location.url && !location.url.startsWith(baseURL)) {
+          return;
+        }
 
-      errors.push(`console.error: ${msg.text()}`);
-    });
+        errors.push(`console.error: ${msg.text()}`);
+      });
+    }
 
     await use(page);
 
