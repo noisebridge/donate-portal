12c0cd99c288d1b94fc52dee47ce97913c5c4b35
Daniel Angell <me@danangell.com>
2026-05-24 01:51:51 -0700

    Add SEO tags to layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -8,8 +8,12 @@ const githubUrl =
   config.gitCommit &&
   (`https://github.com/${config.gitRepo}/tree/${config.gitCommit}` as "safe");
 
+const description =
+  "Support Noisebridge, San Francisco's anarchist hackerspace. Donate to help keep the space open and accessible to all.";
+
 export type LayoutProps = PropsWithChildren<{
   title: string;
+  description?: string;
   script?: string;
   styles?: string;
   isAuthenticated: boolean;
@@ -36,8 +40,35 @@ export function Layout({
             name="viewport"
             content="width=device-width, initial-scale=1.0"
           />
+          <meta name="description" content={description} />
+          <meta name="theme-color" content="#000000" />
           <meta name="stripe-public" content={config.stripePublicKey} />
           <title>{title as "safe"} | Noisebridge</title>
+
+          <meta property="og:type" content="website" />
+          <meta property="og:locale" content="en_US" />
+          <meta property="og:site_name" content="Noisebridge" />
+          <meta
+            property="og:title"
+            content={`${title} | Noisebridge` as "safe"}
+          />
+          <meta property="og:description" content={description} />
+          <meta
+            property="og:image"
+            content={`${config.baseUrl}${assetPath("image/logo.svg")}`}
+          />
+
+          <meta name="twitter:card" content="summary" />
+          <meta
+            name="twitter:title"
+            content={`${title} | Noisebridge` as "safe"}
+          />
+          <meta name="twitter:description" content={description} />
+          <meta
+            name="twitter:image"
+            content={`${config.baseUrl}${assetPath("image/logo.svg")}`}
+          />
+
           <link rel="icon" href={assetPath("image/favicon.svg")} />
           {/**
            * Hide all content initially. This style is reset at the end of
