ee154e4079cfc060b2c4eddec2489336b9c0ace2
Daniel Angell <me@danangell.com>
2026-05-25 17:11:15 -0700

    Increase timeout in stripe.mjs

--- src/assets/js/util/stripe.mjs
+++ src/assets/js/util/stripe.mjs
@@ -345,7 +345,7 @@ export function initCheckoutForm(form, type) {
           Accept: "application/json",
         },
         body: new URLSearchParams(Array.from(new FormData(form).entries())),
-        signal: AbortSignal.timeout(3000),
+        signal: AbortSignal.timeout(10000),
       });
     } catch (e) {
       console.error("Failed to initiate donation:", e);
